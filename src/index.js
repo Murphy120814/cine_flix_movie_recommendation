c42ebb167d619d623719670fe607ec8879d3d3b1
Murphy <109974838+Murphy120814@users.noreply.github.com>
2023-08-24 13:28:33 +0530

    trying to remove conosle

--- src/index.js
+++ src/index.js
@@ -9,6 +9,12 @@ import store from './app/store';
 import App from './components/App';
 import './index.css';
 
+if (process.env.NODE_ENV === 'production') {
+  console.log = () => {};
+  console.error = () => {};
+  console.debug = () => {};
+}
+
 ReactDOM.render(
   <Provider store={store}>
     <ToggleColorModeProvider>
